4c47887c528be5b2da70957941b5dae3b47a8144
Shravika <shravika2056@gmail.com>
2026-07-28 13:03:43 +0530

    Add Evenandoddcount

---
+++ Arrays/EvenAndOddCount.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class EvenAndOddCount {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of the array: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        int evenCount = 0;
+        int oddCount = 0;
+        System.out.println("Enter the array elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+            if (arr[i] % 2 == 0) {
+                evenCount++;
+            } else {
+                oddCount++;
+            }
+        }
+        System.out.println("\nEven Elements: " + evenCount);
+        System.out.println("Odd Elements: " + oddCount);
+        sc.close();
+    }
+}
